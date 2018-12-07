437d6397df2f86bb4d84c06ed762c7e5a9618b8f
justinspar <justinrspar@gmail.com>
2018-12-07 03:34:34 -0500

    10

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -136,7 +136,7 @@ public class HuffProcessor {
 		    HuffNode right = pq.remove();
 		    // create new HuffNode t with weight from
 		    // left.weight+right.weight and left, right subtrees
-		    HuffNode t = new HuffNode(0,left.myWeight+right.myWeight, left, right);
+		    HuffNode t = new HuffNode(-1,left.myWeight+right.myWeight, left, right);
 		    
 		    pq.add(t);
 		}
